3a5192d145525e86fd2daf905d218b9bdc1cd209
Momin Ahmad <mominr082@gmail.com>
2024-10-11 23:48:13 +0530

    added link text widget

---
+++ lib/src/shared/view/link_text.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class LinkText extends StatelessWidget {
+  const LinkText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final RegExp urlRegExp = RegExp(
+      r'(https?:\/\/[^\s]+)',
+      caseSensitive: false,
+    );
+    final match = urlRegExp.firstMatch(text);
+    if (match == null) {
+      return Text(
+        text,
+        style: Theme.of(context).textTheme.bodyMedium,
+      );
+    }
+
+    final url = match.group(0)!;
+    final beforeUrl = text.substring(0, match.start);
+    final afterUrl = text.substring(match.end);
+
+    return RichText(
+      text: TextSpan(
+        style: Theme.of(context).textTheme.bodyMedium,
+        children: [
+          TextSpan(text: beforeUrl),
+          TextSpan(
+            text: url,
+            style: const TextStyle(
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                if (!await launchUrlString(url)) {
+                  if (!context.mounted) return;
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Could not launch $url')),
+                  );
+                }
+              },
+          ),
+          TextSpan(text: afterUrl),
+        ],
+      ),
+    );
+  }
+}
