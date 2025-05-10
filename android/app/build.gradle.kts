ba72ba351675415cbe226892cc17be795023991c
Momin Ahmad <mominr082@gmail.com>
2025-05-10 17:09:38 +0530

    Fix: Update NDK version to a specific value in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.mominraza.messages_wallet"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
